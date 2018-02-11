ff3c5b7ef9e71c436017fbb533cbc9c7bc7bf1a2
Pedro Ávila <pedroavila95@gmail.com>
2018-02-11 18:56:03 -0200

    Test text rendered by App

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,10 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+import { renderComponent, expect } from '../test_helper'
+import App from '../../src/components/app'
 
-describe('App' , () => {
-  let component;
+describe('App', () => {
+  it('shows the correct text', () => {
+    const component = renderComponent(App)
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+    expect(component).to.contain('React simple starter')
+  })
+})
