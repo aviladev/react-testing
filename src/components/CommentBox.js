fdb5a16957f809bd7289344867374383b1611c0e
Pedro Ávila <pedroavila95@gmail.com>
2018-02-12 12:23:11 -0200

    Handle change and submit on CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 
 class CommentBox extends Component {
+  state = { comment: '' }
+
+  handleChange = ({ target: {value} }) =>
+    this.setState({ comment: value })
+  
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.setState({ comment: '' })
+  }
+
   render () {
     return (
-      <div className="comment-box" >
-        <textarea/>
+      <form
+        onSubmit={this.handleSubmit}
+        className="comment-box"
+      >
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.comment} 
+        />
         <button>Submit Comment</button>
-      </div>
+      </form>
     )
   }
 }
